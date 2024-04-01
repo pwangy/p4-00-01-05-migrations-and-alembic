a6bcd9254c901b57ade06b7bd2262625a6fbbe89
pwangy <blipsandclicks@gmail.com>
2024-03-31 21:23:05 -0700

    complete codealong

---
+++ lib/migrations/versions/d176a71a68ce_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: d176a71a68ce
+Revises: 
+Create Date: 2024-03-31 21:11:41.200376
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd176a71a68ce'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
